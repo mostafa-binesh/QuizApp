70e83674521b8f15cbcec37c25c6352b4d5e246b
Mostafa <ubirockteam@gmail.com>
2023-06-10 18:41:00 +0330

    add system model

---
+++ models/system.model.go
@@ -0,0 +1,7 @@
+package models
+
+type System struct {
+	ID        uint       `json:"id" gorm:"primary_key"`
+	Title     string     `json:"title"`
+	Questions []Question `json:"questions" gorm:"foreignKey:SystemID"`
+}
