016f265274fab242323568411988d3254a49bdaf
Mostafa <ubirockteam@gmail.com>
2023-06-10 18:43:31 +0330

    add subject to system model

--- models/system.model.go
+++ models/system.model.go
@@ -4,4 +4,6 @@ type System struct {
 	ID        uint       `json:"id" gorm:"primary_key"`
 	Title     string     `json:"title"`
 	Questions []Question `json:"questions" gorm:"foreignKey:SystemID"`
+	SubjectID uint       `json:"subjectID"`
+	Subject   Subject    `json:"subject"`
 }
