c37ebdddb63200a353a1c86928ddc6cf3ac9bb7e
Mostafa <ubirockteam@gmail.com>
2023-06-22 17:08:31 +0330

    change foreignKey bug in system model

--- models/system.model.go
+++ models/system.model.go
@@ -3,7 +3,8 @@ package models
 type System struct {
 	ID        uint        `json:"id" gorm:"primary_key"`
 	Title     string      `json:"title"`
-	Questions []*Question `json:"questions,omitempty" gorm:"foreignKey:ID"`
+
 	SubjectID uint        `json:"-"`
 	Subject   *Subject    `json:"subject,omitempty"`
+	Questions []*Question `json:"questions,omitempty" gorm:"foreignKey:SystemID"`
 }
