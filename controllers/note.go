7a866d356f9f5711cf9fb41fbacc2073750fc672
Mostafa <ubirockteam@gmail.com>
2023-06-23 17:10:23 +0330

    add editNote in user panel

--- controllers/note.go
+++ controllers/note.go
@@ -37,3 +37,24 @@ func AllNotes(c *fiber.Ctx) error {
 	}
 	return c.JSON(fiber.Map{"data": notes})
 }
+func EditNote(c *fiber.Ctx) error {
+	payload := new(M.EditNoteInput)
+	// parsing the payload
+	if err := c.BodyParser(payload); err != nil {
+		U.ResErr(c, err.Error())
+	}
+	// validation the payload
+	if errs := U.Validate(payload); errs != nil {
+		return c.Status(400).JSON(fiber.Map{"errors": errs})
+	}
+	// get userAnswer with url id param
+	userAnswer := M.UserAnswer{}
+	if err := D.DB().First(&userAnswer, c.Params("id")).Error; err != nil {
+		return U.DBError(c, err)
+	}
+	userAnswer.Note = payload.Note
+	if err := D.DB().Save(&userAnswer).Error; err != nil {
+		return U.DBError(c, err)
+	}
+	return U.ResMessage(c, "Note has been updated")
+}
