700b391fe86c32c73dc05584f32fb123d57e81bd
Mostafa <ubirockteam@gmail.com>
2023-06-25 16:49:44 +0330

    fix quiz and add updateQuiz

--- controllers/quiz.go
+++ controllers/quiz.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	D "docker/database"
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -15,31 +16,36 @@ import (
 
 func AllQuizzes(c *fiber.Ctx) error {
 	user := c.Locals("user").(M.User)
-	quizzes := []M.Quiz{}
-	if err := D.DB().Model(&user).Preload("UserAnswers", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
+	if err := D.DB().Model(&user).Preload("Quizzes.Course").Preload("UserAnswers", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
 		db = db.Order("id ASC")
 		return db
-	}).Preload("UserAnswers.Question.Options").Find(&quizzes).Error; err != nil {
+	}).Preload("Quizzes.UserAnswers.Question.Options").First(&user).Error; err != nil {
 		return U.DBError(c, err)
 	}
 	var userQuizzes []M.QuizToFront
-	for _, quiz := range quizzes {
+	for _, quiz := range user.Quizzes {
+		fmt.Printf("course is %v\n", quiz.Course)
 		userQuizzes = append(userQuizzes, quiz.ConvertQuizToQuizToFront())
 	}
 	return c.JSON(fiber.Map{"data": userQuizzes})
 }
 
 func QuizByID(c *fiber.Ctx) error {
-	quiz := &M.Quiz{}
-	if err := D.DB().Preload("UserAnswers", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
+	// get authenticated user
+	user := c.Locals("user").(M.User)
+	// find quiz with id of paramID of the user with dependencies
+	if result := D.DB().Model(&user).Preload("Quizzes", c.Params("id")).Preload("Quizzes.Course").Preload("UserAnswers", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
 		db = db.Order("id ASC")
 		return db
-	}).Preload("UserAnswers.Question.Options").First(quiz, c.Params("id")).Error; err != nil {
-		return U.DBError(c, err)
+	}).Preload("Quizzes.UserAnswers.Question.Options").First(&user); result.Error != nil {
+		return U.DBError(c, result.Error)
 	}
-	return c.JSON(fiber.Map{
-		"data": quiz.ConvertQuizToQuizToFront(),
-	})
+	// if user quiz with desired id doesn't exist
+	fmt.Printf("user quizzes count: %d\n", len(user.Quizzes))
+	if len(user.Quizzes) <= 0 {
+		return U.ResErr(c, "Quiz not found")
+	}
+	return c.JSON(fiber.Map{"data": user.Quizzes[0].ConvertQuizToQuizToFront()})
 }
 func CreateQuiz(c *fiber.Ctx) error {
 	payload := new(M.QuizInput)
@@ -53,10 +59,12 @@ func CreateQuiz(c *fiber.Ctx) error {
 	}
 	user := c.Locals("user").(M.User)
 	// create the quiz
+	endTime := time.Now().Add(time.Hour * 1)
 	quiz := M.Quiz{
-		UserID:  user.ID,
-		Status:  "pending",
-		EndTime: time.Now().Add(time.Hour * 1),
+		UserID:   user.ID,
+		Status:   "pending",
+		EndTime:  &endTime,
+		CourseID: 1, // TODO hardcoded !
 	}
 	result := D.DB().Create(&quiz)
 	if result.Error != nil {
@@ -64,14 +72,17 @@ func CreateQuiz(c *fiber.Ctx) error {
 	}
 	// create the empty user answer h selected systems
 	// TODO SECURITY ISSUE: checking the id of systems has not been checked
+	// all avaiable questions's id with desired system_id
 	var questionIDs []uint
 	if err := D.DB().Model(&M.Question{}).Distinct().Where("system_id IN (?)", payload.SystemIDs).Pluck("id", &questionIDs).Error; err != nil {
 		return U.DBError(c, err)
 	}
-	var questionCount int64
-	D.DB().Model(&M.Question{}).Count(&questionCount)
 	questionsCount := len(questionIDs)
 	var randomIndex uint
+	fmt.Printf("payload question: %d , available questions count: %d\n", payload.QuestionsCount, questionsCount)
+	if questionsCount <= payload.QuestionsCount {
+		return U.ResErr(c, "not enough questions available")
+	}
 	for i := 0; i < payload.QuestionsCount; i++ {
 		randomIndex = uint(rand.Intn(int(questionsCount)))
 		rand.Seed(time.Now().UnixNano())
@@ -85,21 +96,68 @@ func CreateQuiz(c *fiber.Ctx) error {
 		U.RemoveElementByRef[uint](&questionIDs, int(randomIndex))
 		questionsCount = len(questionIDs)
 	}
+	fmt.Printf("printing the result\n")
 	return c.Status(200).JSON(fiber.Map{
 		"msg":    "Quiz been created",
 		"quizID": quiz.ID,
 	})
 }
+func UpdateQuiz(c *fiber.Ctx) error {
+	payload := new(M.QuizToFront)
+	// parsing the payload
+	if err := c.BodyParser(payload); err != nil {
+		U.ResErr(c, err.Error())
+	}
+	// validation the payload
+	if errs := U.Validate(payload); errs != nil {
+		return c.Status(400).JSON(fiber.Map{"errors": errs})
+	}
+	// get authenticated user info
+	user := c.Locals("user").(M.User)
+	// ! todo maybe can optimize it
+	// get quiz and its dependencies with paramID of the user in order
+	// didn't get the error of ParamsInt, because i checked it in the router
+	quizID, _ := c.ParamsInt("id")
+	if err := D.DB().Model(&user).Preload("Quizzes", quizID).Preload("UserAnswers", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
+		db = db.Order("id ASC")
+		return db
+	}).Preload("Quizzes.UserAnswers.Question.Options").First(&user).Error; err != nil {
+		return U.DBError(c, err)
+	}
+	if !(len(user.Quizzes) == 1) {
+		return U.ResErr(c, "This quiz doesn't exist")
+	}
+	quiz := M.Quiz{}
+	quiz = user.Quizzes[0]
+	convertedUserAnswer := payload.ConvertQuizFrontToQuiz(quiz.UserAnswers)
+	// update the user answers into database
+	fmt.Printf("convertedUserAnswer = %v\n", convertedUserAnswer)
+	if err := D.DB().Save(convertedUserAnswer).Error; err != nil {
+		return U.DBError(c, err)
+	}
+	return c.JSON(fiber.Map{"asd": convertedUserAnswer})
+	return U.ResMessage(c, "Quiz been updated")
+}
 func CreateFakeQuiz(c *fiber.Ctx) error {
-	payload := M.QuizInput{}
-	payload.QuestionsCount = 5
-	payload.SystemIDs = []uint{1, 2, 3, 4, 5}
+	payload := new(M.QuizInput)
+	payload.QuestionsCount = 3
+	payload.SystemIDs = []uint{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// parsing the payload
+	if err := c.BodyParser(payload); err != nil {
+		U.ResErr(c, err.Error())
+	}
+	// validation the payload
+	if errs := U.Validate(payload); errs != nil {
+		return c.Status(400).JSON(fiber.Map{"errors": errs})
+	}
 	user := c.Locals("user").(M.User)
 	// create the quiz
+	endTime := time.Now().Add(time.Hour * 1)
 	quiz := M.Quiz{
-		UserID:  user.ID,
-		Status:  "pending",
-		EndTime: time.Now().Add(time.Hour * 1),
+		UserID:   user.ID,
+		Status:   "pending",
+		EndTime:  &endTime,
+		CourseID: 1, // TODO hardcoded !
 	}
 	result := D.DB().Create(&quiz)
 	if result.Error != nil {
@@ -107,14 +165,17 @@ func CreateFakeQuiz(c *fiber.Ctx) error {
 	}
 	// create the empty user answer h selected systems
 	// TODO SECURITY ISSUE: checking the id of systems has not been checked
+	// all avaiable questions's id with desired system_id
 	var questionIDs []uint
 	if err := D.DB().Model(&M.Question{}).Distinct().Where("system_id IN (?)", payload.SystemIDs).Pluck("id", &questionIDs).Error; err != nil {
 		return U.DBError(c, err)
 	}
-	var questionCount int64
-	D.DB().Model(&M.Question{}).Count(&questionCount)
 	questionsCount := len(questionIDs)
 	var randomIndex uint
+	fmt.Printf("payload question: %d , available questions count: %d\n", payload.QuestionsCount, questionsCount)
+	if questionsCount <= payload.QuestionsCount {
+		return U.ResErr(c, "not enough questions available")
+	}
 	for i := 0; i < payload.QuestionsCount; i++ {
 		randomIndex = uint(rand.Intn(int(questionsCount)))
 		rand.Seed(time.Now().UnixNano())
@@ -128,14 +189,9 @@ func CreateFakeQuiz(c *fiber.Ctx) error {
 		U.RemoveElementByRef[uint](&questionIDs, int(randomIndex))
 		questionsCount = len(questionIDs)
 	}
-	quizToFront := &M.Quiz{}
-	if err := D.DB().Preload("UserAnswers", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
-		db = db.Order("id ASC")
-		return db
-	}).Preload("UserAnswers.Question.Options").Last(quizToFront).Error; err != nil {
-		return U.DBError(c, err)
-	}
-	return c.JSON(fiber.Map{
-		"data": quizToFront.ConvertQuizToQuizToFront(),
+	fmt.Printf("printing the result\n")
+	return c.Status(200).JSON(fiber.Map{
+		"msg":    "Quiz been created",
+		"quizID": quiz.ID,
 	})
 }
