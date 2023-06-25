95ee4118cba6ee015e3d116930d12a3349707d4d
Mostafa <ubirockteam@gmail.com>
2023-06-25 16:33:53 +0330

    add course to question model directly

--- models/question.model.go
+++ models/question.model.go
@@ -10,8 +10,6 @@ import (
 
 type Question struct {
 	ID uint `json:"no" gorm:"primary_key"`
-	// CourseID uint    `json:"-"`
-	// Course   *Course `json:"course,omitempty" gorm:"foreignKey:CourseID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
 	Title       string  `json:"question"`
 	Status      string  `json:"-"`
 	Description string  `json:"description"`
@@ -20,6 +18,10 @@ type Question struct {
 	Options  []*Option `json:"options,omitempty"`
 	SystemID uint      `json:"-"`
 	System   *System   `json:"system,omitempty" gorm:"foreignKey:SystemID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
+	// although we could get the course id from question >subject > system, but that would
+	//  cost resource, i rather add a courseID to the Question table and get it directly
+	CourseID *uint    `json:"-"`
+	Course   *Course `json:"course,omitempty" gorm:"foreignKey:CourseID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
 }
 
 //
