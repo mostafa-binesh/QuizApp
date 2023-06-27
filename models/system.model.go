5549e8f1cf86c58106d6c5fdfb5874b571f1d5eb
Mostafa <ubirockteam@gmail.com>
2023-06-28 01:27:25 +0330

    as frontend guys asked, added subjectID to model

--- models/system.model.go
+++ models/system.model.go
@@ -3,7 +3,7 @@ package models
 type System struct {
 	ID        uint        `json:"id" gorm:"primary_key"`
 	Title     string      `json:"title"`
-	SubjectID uint        `json:"-"`
+	SubjectID uint        `json:"subjectID"`
 	Subject   *Subject    `json:"subject,omitempty"`
 	Questions []*Question `json:"questions,omitempty" gorm:"foreignKey:SystemID"`
 }
