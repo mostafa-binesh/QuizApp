38671f76b6b27f2c432d484d1d552e52e6e967b0
Mostafa <ubirockteam@gmail.com>
2023-06-28 00:00:19 +0330

    add question controller- all questions function

---
+++ controllers/question.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	D "docker/database"
+	M "docker/models"
+	U "docker/utils"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func AllQuestions(c *fiber.Ctx) error {
+	// select all userAnswers of the authenticated user
+	user := c.Locals("user").(M.User)
+	if err := D.DB().Model(&user).
+		Preload("Quizzes.UserAnswers.Question", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
+			// if filterType is body and search query exists, search in question's title
+			if c.Query("filterBy") == "body" && c.Query("search") != "" {
+				db = db.Where("title = ?", c.Query("search"))
+			}
+			return db
+		}).
+		Preload("Quizzes.UserAnswers.Question.System", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
+			// if filterType is system and search query exists, search in subject's title
+			if c.Query("filterBy") == "system" && c.Query("search") != "" {
+				db = db.Where("title = ?", c.Query("search"))
+			}
+			return db
+		}).Preload("Quizzes.UserAnswers.Question.System.Subject", func(db *gorm.DB) *gorm.DB { // could do this as well : Preload("Comments", "ORDER BY ? ASC > ?", "id")
+		// if filterType is subject and search query exists, search in question's title
+		if c.Query("filterBy") == "subject" && c.Query("search") != "" {
+			db = db.Where("title = ?", c.Query("search"))
+		}
+		return db
+	}).Find(&user).Error; err != nil {
+		return U.DBError(c, err)
+	}
+	var questions []M.QuestionSearch
+	for _, quiz := range user.Quizzes {
+		for _, answer := range quiz.UserAnswers {
+			questions = append(questions, M.QuestionSearch{
+				ID:      0,
+				Subject: answer.Question.System.Subject.Title,
+				System:  answer.Question.System.Title,
+			})
+		}
+	}
+	return c.JSON(fiber.Map{"data": questions})
+}
