e9d1add70fee56f7e9d2c65c89b55e0c9185c18b
Mostafa <ubirockteam@gmail.com>
2023-06-28 01:51:30 +0330

    add quizID to answerNote struct

--- models/userAnswer.model.go
+++ models/userAnswer.model.go
@@ -4,15 +4,15 @@ type UserAnswer struct {
 	ID         uint      `json:"id,omitempty" gorm:"primary_key"`
 	QuestionID uint      `json:"-"`
 	Question   *Question `json:"question,omitempty" gorm:"foreignKey:QuestionID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
-	Note       *string   `json:"note,omitempty"`
-	IsMarked   bool      `json:"isMarked,omitempty" gorm:"default:false;"`
-	Submitted  bool      `json:"submitted,omitempty" gorm:"default:false;"`
-	Status     string    `json:"status,omitempty"`
-	SpentTime  uint      `json:"spentTime,omitempty"`
-	UserID     uint      `json:"-"`
-	User       *User     `json:"user,omitempty" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
-	QuizID     uint      `json:"-"`
-	Quiz       *Quiz     `json:"quiz,omitempty" gorm:"foreignKey:QuizID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
+	Note       *string `json:"note,omitempty"`
+	IsMarked   bool    `json:"isMarked,omitempty" gorm:"default:false;"`
+	Submitted  bool    `json:"submitted,omitempty" gorm:"default:false;"`
+	Status     string  `json:"status,omitempty"`
+	SpentTime  uint    `json:"spentTime,omitempty"`
+	UserID     uint    `json:"-"`
+	User       *User   `json:"user,omitempty" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
+	QuizID     uint    `json:"-"`
+	Quiz       *Quiz   `json:"quiz,omitempty" gorm:"foreignKey:QuizID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
 	// ! multiple-choice option
 	// Answers    []*Answer `json:"answers,omitempty"`
 	// ! single-choice option
@@ -21,6 +21,7 @@ type UserAnswer struct {
 type AnswerNote struct {
 	ID         uint    `json:"id"` // is parent UserAnswer's ID
 	QuestionID uint    `json:"questionID"`
+	QuizID     uint    `json:"quidID"`
 	Note       *string `json:"note"`
 }
 type EditNoteInput struct {
