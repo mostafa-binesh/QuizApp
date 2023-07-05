fc48687f54d305e920636cefc8f7c223721a5970
Mostafa <ubirockteam@gmail.com>
2023-07-05 19:49:58 +0330

    change admin question input, made options dynamic and implemented images polymorphic relationship

--- controllers/admin/admin.question.go
+++ controllers/admin/admin.question.go
@@ -22,37 +22,41 @@ func CreateQuestion(c *fiber.Ctx) error {
 	if errs := U.Validate(payload); errs != nil {
 		return c.Status(400).JSON(fiber.Map{"errors": errs})
 	}
-	// init options array
-	options := []*M.Option{}
-	options = append(options, &M.Option{Title: payload.Option1, Index: "A"})
-	options = append(options, &M.Option{Title: payload.Option2, Index: "B"})
-	options = append(options, &M.Option{Title: payload.Option3, Index: "C"})
-	options = append(options, &M.Option{Title: payload.Option4, Index: "D"})
-	// if first option is correct, client needs to send 1
-	options[payload.CorrectOption-1].IsCorrect = true
+	// handling options
+	questionOptions := []M.Option{}
+	// go through each payload.option and convert it into M.Option
+	for i, option := range payload.Options {
+		questionOptions = append(questionOptions, M.Option{
+			Title:     option.Title,
+			Index:     U.GetNthAlphabeticUpperLetter(i + 1),
+			IsCorrect: option.IsCorrect,
+		}) 
+	}
 	// get images from request
 	form, err := c.MultipartForm()
 	images := form.File["images"]
-	if err != nil {
+	if err != nil || images == nil {
 		return U.ResErr(c, err.Error())
 	}
 	fmt.Printf("images variable %v\n", images)
-	// create appropriate name for images and save them int disc
-	var imagesNames []string
+	// create appropriate unique name for images and save them int disc
+	var questionImages []M.Image
 	for _, image := range images {
 		fmt.Printf("image name: %v\n", image.Filename)
 		uuid := uuid.New().String()
 		newFileName := uuid + "-" + image.Filename
 		c.SaveFile(image, fmt.Sprintf(U.UploadLocation+"/%s", newFileName))
-		imagesNames = append(imagesNames, newFileName)
+		questionImages = append(questionImages, M.Image{
+			Name: newFileName,
+		})
 	}
 	// create new question with given info
 	newQuestion := M.Question{
 		Title:       payload.Question,
-		Options:     options,
+		Options:     questionOptions,
 		SystemID:    payload.SystemID,
 		Description: payload.Description,
-		// Image:       imagesNames,
+		Images:      questionImages,
 	}
 	// insert new question to the database
 	result := D.DB().Create(&newQuestion)
@@ -63,7 +67,7 @@ func CreateQuestion(c *fiber.Ctx) error {
 }
 func QuestionByID(c *fiber.Ctx) error {
 	question := &M.Question{}
-	if err := D.DB().Preload("Course").Preload("System.Subject").First(question, c.Params("id")).Error; err != nil {
+	if err := D.DB().Preload("Course").Preload("Images").Preload("System.Subject").First(question, c.Params("id")).Error; err != nil {
 		return U.DBError(c, err)
 	}
 	return c.JSON(fiber.Map{"data": question})
