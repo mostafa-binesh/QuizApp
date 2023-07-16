420e16f5948a5fc82bb6b6f79332470cb0e51540
Mostafa <ubirockteam@gmail.com>
2023-07-16 21:05:46 +0330

    change tab model to save Tables as json not a separated table

--- models/tab.model.go
+++ models/tab.model.go
@@ -1,8 +1,23 @@
 package models
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+)
+
+type TabArray []Tab
 type Tab struct {
 	ID         uint      `json:"no" gorm:"primary_key"`
-	// Tables     []Table   `json:"tables"`
+	Tables     TabArray  `json:"tables" gorm:"type:text"`
 	QuestionID uint      `json:"-"`
 	Question   *Question `json:"question,omitempty" gorm:"foreignKey:QuestionID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
+}
+
+func (sla *TabArray) Scan(src interface{}) error {
+	return json.Unmarshal(src.([]byte), &sla)
+}
+
+func (sla TabArray) Value() (driver.Value, error) {
+	val, err := json.Marshal(sla)
+	return string(val), err
 }
