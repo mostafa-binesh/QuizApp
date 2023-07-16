61a895dec3d51ba3d55ce0f98e2bdfb7224eb92d
Mostafa <ubirockteam@gmail.com>
2023-07-17 00:14:31 +0330

    add MarshalJSON method to option model

--- models/option.model.go
+++ models/option.model.go
@@ -1,5 +1,7 @@
 package models
 
+import "encoding/json"
+
 type Option struct {
 	ID        uint   `json:"id" gorm:"primary_key"`
 	Title     string `json:"option,omitempty"`
@@ -13,12 +15,44 @@ type AdminOptionInput struct {
 	Title     string `form:"title" validate:"required"`
 	IsCorrect bool   `form:"isCorrect" validate:"required"`
 }
+
+// FrontOption model has a dedicated MarshalJSON method
 type FrontOption struct {
-	ID            uint   `json:"id" gorm:"primary_key"`
-	Title         string `json:"option,omitempty"`
-	Index         string `json:"index,omitempty"`
-	IsCorrectUint *uint  `json:"status,omitempty"`
-	IsCorrectBool *bool  `json:"status,omitempty"`
+	ID            uint
+	Title         string
+	Index         string
+	IsCorrectUint *uint
+	IsCorrectBool *bool
+}
+
+func (fo FrontOption) MarshalJSON() ([]byte, error) {
+	// If IsCorrectUint is not nil, include IsCorrectBool in the JSON output
+	if fo.IsCorrectUint != nil {
+		return json.Marshal(struct {
+			ID            uint   `json:"id"`
+			Title         string `json:"option,omitempty"`
+			Index         string `json:"index,omitempty"`
+			IsCorrectUint *uint  `json:"status,omitempty"`
+		}{
+			ID:            fo.ID,
+			Title:         fo.Title,
+			Index:         fo.Index,
+			IsCorrectUint: fo.IsCorrectUint,
+		})
+	}
+
+	// If IsCorrectUint is nil, omit IsCorrectBool from the JSON output
+	return json.Marshal(struct {
+		ID            uint   `json:"id"`
+		Title         string `json:"option,omitempty"`
+		Index         string `json:"index,omitempty"`
+		IsCorrectBool *bool  `json:"status,omitempty"`
+	}{
+		ID:            fo.ID,
+		Title:         fo.Title,
+		Index:         fo.Index,
+		IsCorrectBool: fo.IsCorrectBool,
+	})
 }
 func ConvertOptionToFrontOption(options *[]Option, questionType QuestionType) *[]FrontOption {
 	frontOptions := make([]FrontOption, len(*options))
