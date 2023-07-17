97581b44b66b462221c57ab219dacc15698538db
Mostafa <ubirockteam@gmail.com>
2023-07-17 16:39:07 +0330

    fix scan method of Tab model and TabArray field

--- models/tab.model.go
+++ models/tab.model.go
@@ -3,21 +3,33 @@ package models
 import (
 	"database/sql/driver"
 	"encoding/json"
+	// "errors"
+	"fmt"
 )
 
-type TabArray []Tab
+type TableArray []Table
 type Tab struct {
-	ID         uint      `json:"no" gorm:"primary_key"`
-	Tables     TabArray  `json:"tables" gorm:"type:text"`
-	QuestionID uint      `json:"-"`
-	Question   *Question `json:"question,omitempty" gorm:"foreignKey:QuestionID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
+	ID         uint       `json:"no" gorm:"primary_key"`
+	Tables     TableArray `json:"tables" gorm:"type:text"`
+	QuestionID uint       `json:"-"`
+	Question   *Question  `json:"question,omitempty" gorm:"foreignKey:QuestionID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
 }
 
-func (sla *TabArray) Scan(src interface{}) error {
-	return json.Unmarshal(src.([]byte), &sla)
-}
-
-func (sla TabArray) Value() (driver.Value, error) {
+func (sla TableArray) Value() (driver.Value, error) {
 	val, err := json.Marshal(sla)
 	return string(val), err
+}
+
+// Scan Unmarshal
+func (a *TableArray) Scan(value interface{}) error {
+	var bytes []byte
+	switch v := value.(type) {
+	case []byte:
+		bytes = v
+	case string:
+		bytes = []byte(v)
+	default:
+		return fmt.Errorf("failed to unmarshal JSONB value: %#v", value)
+	}
+	return json.Unmarshal(bytes, &a)
 }
