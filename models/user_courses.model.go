772dfe6b9c03fc31776273d2a8742e0115ebab1b
mostafa-binesh <ubirockteam@gmail.com>
2023-08-05 15:55:28 +0330

    add course ref to CourseUser model and UserNonExpiredCourses db scope

--- models/user_courses.model.go
+++ models/user_courses.model.go
@@ -7,18 +7,23 @@ import (
 )
 
 type CourseUser struct {
-	UserID         int `gorm:"foreignKey:ID"`
-	CourseID       int `gorm:"foreignKey:ID"`
+	ID             uint   `json:"id" gorm:"primary_key"`
+	UserID         int    `gorm:"foreignKey:ID"`
+	CourseID       int    `gorm:"foreignKey:ID"`
+	Course         Course `json:"course"`
 	ExpirationDate time.Time
 }
 
-// Set the table name for the join table
+// Set the table name
+// if don't set it, it will set "course_users" name for the table
 func (CourseUser) TableName() string {
 	return "course_user"
 }
 
-// Custom global scope for UserCourse model to filter out expired records
-func NonExpiredCourses(db *gorm.DB) *gorm.DB {
-	currentTime := time.Now()
-	return db.Where("expiration_date > ?", currentTime)
+// Custom global scope for CourseUser model to filter out expired records
+func UserNonExpiredCourses() func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		currentTime := time.Now()
+		return db.Where("expiration_date > ?", currentTime)
+	}
 }
