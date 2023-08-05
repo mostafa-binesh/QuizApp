191fdff50ba1492d74281dadce44dd17ca12a6b2
mostafa-binesh <ubirockteam@gmail.com>
2023-08-05 15:47:11 +0330

    add ImportCoursesFromWoocommerce function to wc service

--- services/woocommerce.service.go
+++ services/woocommerce.service.go
@@ -1,10 +1,14 @@
 package services
 
 import (
+	D "docker/database"
+	M "docker/models"
 	U "docker/utils"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 // import (
@@ -126,4 +130,84 @@ func ConvertWCTimeToStandard(wcTime string) time.Time {
 	time, _ := time.Parse(layout, wcTime)
 	return time
 
+}
+
+// gets all products from wc. api and save them into the database
+func ImportCoursesFromWoocommerce() (*[]M.Course, error) {
+	// get all woocommerce products from its service
+	wcCourses, err := GetAllProducts()
+	if err != nil {
+		return nil, err
+	}
+	// convert the wooc. retreived products into course model
+	convertedCourses, err := ConvertWCCourseToCourseModel(&wcCourses)
+	if err != nil {
+		return nil, err
+	}
+	// because some courses need to find reference to their parent in the database
+	// and maybe it's the first time that the parent course is being inserted into the database
+	// we need to insert all of courses into the database first and ignore all errors
+	for _, course := range *convertedCourses {
+		// for some reason, some of retreived courses were empty
+		if course.Title == "" {
+			continue
+		}
+		// saving the courses to the database
+		// not using create func. to create courses to the db
+		// becausew some of courses variables such as duration may been changed
+		D.DB().Save(&course)
+	}
+	// create a map to store parent courses id and its courses
+	DBCourseWoocommerceIDMap := make(map[uint]*M.Course)
+	for _, course := range *convertedCourses {
+		// for some reason, some of retreived courses were empty
+		if course.Title == "" {
+			continue
+		}
+		var existingCourse M.Course
+		var parentCourse M.Course
+		// Try to find the course with the given woocommerce_id
+		wcCoruseResult := D.DB().Where("woocommerce_id = ?", course.WoocommerceID).First(&existingCourse)
+		// also need to find the parent course with the given course.parentid
+		// NOTE that course.ParentID comes from woocommerce !
+		if course.ParentID != nil {
+			// if the parent id wasn't submitted in the map before
+			// try to find the parent dbCourse and submit the parent id record
+			if DBCourseWoocommerceIDMap[*course.ParentID] == nil {
+				// parent course map doesn't exist in the map
+				// try to find it in the database
+				// parentResult := D.DB().First(&parentCourse, *course.ParentID)
+				parentResult := D.DB().Where("woocommerce_id = ?", course.ParentID).First(&parentCourse)
+				// if the parent exist
+				if parentResult.RowsAffected == 1 {
+					DBCourseWoocommerceIDMap[*course.ParentID] = &parentCourse
+				}
+			} else {
+				parentCourse = *DBCourseWoocommerceIDMap[*course.ParentID]
+			}
+		}
+
+		// if course with desired woocommerce_id not found, we need to create one
+		if wcCoruseResult.RowsAffected == 0 {
+			// if parent exist, set parentID to dbCourse id
+			if parentCourse.Title != "" {
+				course.ParentID = &parentCourse.ID
+			}
+			course.Duration = parentCourse.Duration
+			// insert it to the database
+			D.DB().Create(&course)
+		} else {
+			// if found, just update it
+			// course is convereted wc course
+			existingCourse.Title = course.Title
+			if parentCourse.Title != "" {
+				existingCourse.ParentID = &parentCourse.ID
+				existingCourse.Duration = parentCourse.Duration
+			}
+			existingCourse.ValidityDaysPeriod = course.ValidityDaysPeriod
+			// update it to db
+			D.DB().Save(&existingCourse)
+		}
+	}
+	return convertedCourses, nil
 }
