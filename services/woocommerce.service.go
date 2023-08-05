220c3563bd18bdf3e39d6e8659f68af7ed30d13c
mostafa-binesh <ubirockteam@gmail.com>
2023-08-05 15:46:32 +0330

    add ConvertWCCourseToCourseModel function

--- services/woocommerce.service.go
+++ services/woocommerce.service.go
@@ -18,8 +18,8 @@ type WCProduct struct {
 	Attributes []WCProductAttributes `json:"attributes"`
 }
 type WCProductAttributes struct {
-	Name    string   `json:"name"`
-	Options []string `json:"options"`
+	Name    string    `json:"name"`
+	Options []*string `json:"options"`
 }
 
 // in wc. products model, we have serveral attributes:
@@ -85,4 +85,45 @@ func GetProductByID(productID int) (WCProduct, error) {
 	}
 
 	return product, nil
+}
+func ConvertWCCourseToCourseModel(wcCourses *[]WCProduct) (*[]M.Course, error) {
+	courses := make([]M.Course, len(*wcCourses))
+	for _, wcCourse := range *wcCourses {
+		newCourse := M.Course{}
+		for _, att := range wcCourse.Attributes {
+			if len(att.Options) != 1 || att.Options[0] == nil {
+				continue
+			}
+			// todo : change second and third conditions to else if
+			// > because an attr. can only have one value of the checked ones
+			// duration is only for parent course
+			if att.Name == "Duration" {
+				newCourse.Duration, _ = strconv.ParseUint(*att.Options[0], 10, 32)
+			}
+			// parentCourse and ValidityPeriod is only for children courses
+			if att.Name == "ParentID" {
+				parsedUint64, _ := strconv.ParseUint(*att.Options[0], 10, 64)
+				parsedUint := uint(parsedUint64)
+				newCourse.ParentID = &parsedUint
+			}
+			if att.Name == "ValidityPeriod" {
+				parsedUint64, _ := strconv.ParseUint(*att.Options[0], 10, 64)
+				newCourse.ValidityDaysPeriod = uint(parsedUint64)
+			}
+		}
+		newCourse.WoocommerceID = uint(wcCourse.ID)
+		newCourse.Title = wcCourse.Name
+		courses = append(courses, newCourse)
+	}
+	return &courses, nil
+}
+
+// converts woocommerce time string to time.Time
+func ConvertWCTimeToStandard(wcTime string) time.Time {
+	// Layout defines the format of the input string
+	layout := "2006-01-02T15:04:05"
+	// Parse the input string and convert it to a time.Time object
+	time, _ := time.Parse(layout, wcTime)
+	return time
+
 }
