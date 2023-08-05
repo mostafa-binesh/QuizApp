aef243f543d0482443c0a28de3905a0a16226c9e
mostafa-binesh <ubirockteam@gmail.com>
2023-08-05 15:47:49 +0330

    update course seeder using its service

--- database/seeders/course.seeder.go
+++ database/seeders/course.seeder.go
@@ -1,23 +1,13 @@
 package seeders
 
 import (
-	D "docker/database"
-	M "docker/models"
+	S "docker/services"
 )
 
-func CourseSeeder() {
-	medicalCourses := []string{
-        "Anatomy and Physiology",
-        "Pharmacology",
-        "Medical Terminology",
-        "Pathophysiology",
-        "Medical Ethics",
-		"NC-LEX RN", // main course , id  = 5
-    }
-	for i, course := range medicalCourses {
-		D.DB().Create(&M.Course{
-			Title: course,
-			WoocommerceID: uint(i+1),
-		})
+func CourseSeeder() error {
+	_, err := S.ImportCoursesFromWoocommerce()
+	if err != nil {
+		return err
 	}
+	return nil
 }
