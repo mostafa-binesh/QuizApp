3385e3a5abb87e392d5980576604d4f2cd5e6b10
mostafa-binesh <ubirockteam@gmail.com>
2023-08-07 18:59:06 +0330

    prioritzing system env to .env var in env f. utility

--- utils/env.util.go
+++ utils/env.util.go
@@ -12,15 +12,15 @@ import (
 // if there was no env. variable, panics
 func Env(key string) string {
 	// load .env file
-	err := env.Load(".env")
-	if err != nil {
-		value, ok := os.LookupEnv(key)
-		if !ok {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		err := env.Load(".env")
+		if err != nil {
 			panic(fmt.Sprintf("can't get env variable: %s", key))
 		}
-		return value
+		return os.Getenv(key)
 	}
-	return os.Getenv(key)
+	return value
 }
 
 // returns true if key variable in .env file is "true"
