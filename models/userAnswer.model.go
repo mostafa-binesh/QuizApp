fa2515c8917e164be3a95a5218973122e1293e66
mostafa-binesh <ubirockteam@gmail.com>
2023-09-02 13:07:02 +0330

    refactor IsChosenOptionsCorrect function in userAnswer model and minor changes

--- models/userAnswer.model.go
+++ models/userAnswer.model.go
@@ -1,5 +1,7 @@
 package models
 
+import "strings"
+
 type UserAnswer struct {
 	ID         uint      `json:"id,omitempty" gorm:"primary_key"`
 	QuestionID uint      `json:"-"`
@@ -30,6 +32,7 @@ type EditNoteInput struct {
 }
 
 // checks answer is true, false or empty
+// ! answer.question.options must be preloaded
 func CalculateAnswerStats(answer UserAnswer) (correctAnswerCount, incorrectAnswerCount, omittedAnswerCount uint) {
 	// if answer is empty, increase omittedAnswerCount by one and return stats
 	if answer.Answer == nil {
@@ -53,28 +56,52 @@ func CalculateAnswerStats(answer UserAnswer) (correctAnswerCount, incorrectAnswe
 // array version of CalculateAnswerStats
 // calculate correct, incorrect and omitted answers count of userAnswers
 func CalculateAnswersStats(answers []UserAnswer) (correctAnswerCount, incorrectAnswerCount, omittedAnswerCount uint) {
-	var found bool
 	for _, answer := range answers {
-		found = false
-		if answer.Answer == nil {
-			omittedAnswerCount++
-			continue
-		}
-		for _, option := range answer.Question.Options {
-			// had to make option a separated value to be able to compare with answer.answer
+		correct, incorrect, omitted := CalculateAnswerStats(answer)
+		correctAnswerCount += correct
+		incorrectAnswerCount += incorrect
+		omittedAnswerCount += omitted
+	}
+	return
+}
+
+// checks if answer is correct, incorrect or null
+// ! not checking the "IsCorrect" field
+// ! answer.Question.Options must be preloaded
+func (answer UserAnswer) IsChosenOptionsCorrect() *bool {
+	// func IsAnswerCorrect(answer UserAnswer) *bool {
+	// if answer is empty, return nil
+	var isCorrect bool
+	if answer.Answer == nil {
+		return nil
+	}
+	// # main logic
+	// if answer is not empty, find out that answer is correct or not
+
+	var CorrectAnswersCheckedCount uint
+	CorrectAnswersRequiredCount := answer.Question.CorrectOptionsCount()
+	splittedAnswers := answer.SplittedAnswers()
+	// why priorize option to answer : what if user's answers are "E,E,E" for any reason?, algorithm is vunleable against it
+	for _, option := range answer.Question.Options {
+		for _, chosenAnswerIndex := range splittedAnswers {
 			optionIndex := option.Index
-			userAnswerIndex := answer.Answer
-			if *userAnswerIndex == optionIndex {
-				correctAnswerCount++
-				found = true
+			if chosenAnswerIndex == optionIndex {
+				CorrectAnswersCheckedCount++
 				break
 			}
 		}
-		if !found {
-			incorrectAnswerCount++
-		}
 	}
-	return
+	// return the final result
+	isCorrect = CorrectAnswersRequiredCount == CorrectAnswersCheckedCount
+	return &isCorrect
+}
+func (answer UserAnswer) SplittedAnswers() []string {
+	if answer.Answer != nil {
+		unSplittedAnswer := *answer.Answer
+		splittedAnswers := strings.Split(unSplittedAnswer, ",")
+		return splittedAnswers
+	}
+	return []string{}
 }
 
 // this model has been used in overall report handler
