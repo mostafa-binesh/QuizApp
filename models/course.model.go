addba121d562a1258749de319ddbf1cff2540b62
Mostafa <ubirockteam@gmail.com>
2023-10-04 23:07:07 +0330

    seperate nextGen and trad. questions

--- models/course.model.go
+++ models/course.model.go
@@ -27,18 +27,23 @@ type CourseWithExpirationDate struct {
 	ExpirationDate time.Time  `json:"expirationDate"`
 	Duration       uint64     `json:"duration"`
 }
+type NestedSubjectsWithQuestionsCount struct {
+	Subjects []SubjectWithQuestionsCount `json:"subjects"`
+}
 type CourseWithExpirationDateAndQuestionsCount struct {
-	ID                           uint                         `json:"id"`
-	Title                        string                       `json:"title"`
-	Subjects                     []*SubjectWithQuestionsCount `json:"subjects"`
-	ExpirationDate               time.Time                    `json:"expirationDate"`
-	Duration                     uint64                       `json:"duration"`
-	QuestionsCount               int                          `json:"questionsCount"`
-	TraditionalQuestionsCount    int                          `json:"traditionalQuestionsCount"`
-	NextGenerationQuestionsCount int                          `json:"nextGenerationQuestionsCount"`
-	MarkedQuestionsCount         uint                         `json:"markedQuestionsCount"`
-	CorrectQuestionsCount        uint                         `json:"correctQuestionsCount"`
-	IncorrectQuestionsCount      uint                         `json:"incorrectQuestionsCount"`
+	ID    uint   `json:"id"`
+	Title string `json:"title"`
+	// Subjects                     []*SubjectWithQuestionsCount `json:"subjects"`
+	TraditionalSubjects          NestedSubjectsWithQuestionsCount `json:"traditionalSubjects"`
+	NextGenerationSubjects       NestedSubjectsWithQuestionsCount `json:"nextGenerationSubjects"`
+	ExpirationDate               time.Time                        `json:"expirationDate"`
+	Duration                     uint64                           `json:"duration"`
+	QuestionsCount               int                              `json:"questionsCount"`
+	TraditionalQuestionsCount    int                              `json:"traditionalQuestionsCount"`
+	NextGenerationQuestionsCount int                              `json:"nextGenerationQuestionsCount"`
+	MarkedQuestionsCount         uint                             `json:"markedQuestionsCount"`
+	CorrectQuestionsCount        uint                             `json:"correctQuestionsCount"`
+	IncorrectQuestionsCount      uint                             `json:"incorrectQuestionsCount"`
 }
 
 // model used for creating new course
@@ -53,7 +58,7 @@ type CourseWithTitleOnly struct {
 type CourseWithQuestionsCount struct {
 	ID                           uint                         `json:"id"`
 	Title                        string                       `json:"title"`
-	Subjects                     []*SubjectWithQuestionsCount `json:"subjects"`
+	Subjects                     []SubjectWithQuestionsCount `json:"subjects"`
 	Duration                     uint64                       `json:"-"` // todo don't show it for now, fix it later
 	QuestionsCount               int                          `json:"questionsCount"`
 	TraditionalQuestionsCount    int                          `json:"traditionalQuestionsCount"`
@@ -85,8 +90,8 @@ func ConvertCourseToCourseWithQuestionsCounts(course Course) (coursesWithQuestio
 	var subjectsQuestionsCount int
 	var subjectTraditionalsCount int
 	var subjectNextGenerationsCount int
-	var systemWithQuestionsCount []*SystemWithQuestionsCount
-	var subjectsWithQuestionsCount []*SubjectWithQuestionsCount
+	var systemWithQuestionsCount []SystemWithQuestionsCount
+	var subjectsWithQuestionsCount []SubjectWithQuestionsCount
 	for _, subject := range course.Subjects {
 		systemWithQuestionsCount = nil
 		systemsQuestionsCount = 0
@@ -97,7 +102,7 @@ func ConvertCourseToCourseWithQuestionsCounts(course Course) (coursesWithQuestio
 			traditionalCount, nextGenerationCount := system.QuestionsCount()
 			systemTraditionalsCount += traditionalCount
 			systemNextGenerationsCount += nextGenerationCount
-			systemWithQuestionsCount = append(systemWithQuestionsCount, &SystemWithQuestionsCount{
+			systemWithQuestionsCount = append(systemWithQuestionsCount, SystemWithQuestionsCount{
 				ID:                           system.ID,
 				Title:                        system.Title,
 				SubjectID:                    system.SubjectID,
@@ -109,7 +114,7 @@ func ConvertCourseToCourseWithQuestionsCounts(course Course) (coursesWithQuestio
 		subjectsQuestionsCount += systemsQuestionsCount
 		subjectTraditionalsCount += systemTraditionalsCount
 		subjectNextGenerationsCount += systemNextGenerationsCount
-		subjectsWithQuestionsCount = append(subjectsWithQuestionsCount, &SubjectWithQuestionsCount{
+		subjectsWithQuestionsCount = append(subjectsWithQuestionsCount, SubjectWithQuestionsCount{
 			ID:                           subject.ID,
 			Title:                        subject.Title,
 			Systems:                      systemWithQuestionsCount,
@@ -177,7 +182,8 @@ func UserHasCourse(userID uint, courseID uint) (bool, error) {
 	}
 	return U.ExistsInArray[uint](courseIDs, courseID), nil
 }
-// this function can be optimized very easily 
+
+// this function can be optimized very easily
 // just need to remove the subjects.systems.question preload and use the parentCourse.Questions preload
 func UserBoughtCoursesWithExpirationDateAndQuestionsCount(userID uint) (*[]CourseWithExpirationDateAndQuestionsCount, error) {
 	var userBoughtCourses []CourseUser
@@ -199,7 +205,58 @@ func UserBoughtCoursesWithExpirationDateAndQuestionsCount(userID uint) (*[]Cours
 	var correctCount uint
 	var incorrectCount uint
 	var markedCount uint
+
+	var systemsQuestionsCount int
+	var systemTraditionalsCount int
+	var systemNextGenerationsCount int
+	var subjectsQuestionsCount int
+	var subjectTraditionalsCount int
+	var subjectNextGenerationsCount int
+	var systemWithQuestionsCount []SystemWithQuestionsCount
+	var subjectsWithQuestionsCount []SubjectWithQuestionsCount
+
+	
+
+	var TraditionalSubjects NestedSubjectsWithQuestionsCount
+	var NextGenerationSubjects NestedSubjectsWithQuestionsCount
 	for i := 0; i < len(userBoughtCourses); i++ {
+		for _, subject := range userBoughtCourses[i].Course.Subjects {
+			systemWithQuestionsCount = nil
+			systemsQuestionsCount = 0
+			systemTraditionalsCount = 0
+			systemNextGenerationsCount = 0
+			for _, system := range subject.Systems {
+				systemsQuestionsCount += len(system.Questions)
+				traditionalCount, nextGenerationCount := system.QuestionsCount()
+				systemTraditionalsCount += traditionalCount
+				systemNextGenerationsCount += nextGenerationCount
+				systemWithQuestionsCount = append(systemWithQuestionsCount, SystemWithQuestionsCount{
+					ID:                           system.ID,
+					Title:                        system.Title,
+					SubjectID:                    system.SubjectID,
+					QuestionsCount:               len(system.Questions),
+					TraditionalQuestionsCount:    int(traditionalCount),
+					NextGenerationQuestionsCount: int(nextGenerationCount),
+				})
+			}
+			subjectsQuestionsCount += systemsQuestionsCount
+			subjectTraditionalsCount += systemTraditionalsCount
+			subjectNextGenerationsCount += systemNextGenerationsCount
+			subjectsWithQuestionsCount = append(subjectsWithQuestionsCount, SubjectWithQuestionsCount{
+				ID:                           subject.ID,
+				Title:                        subject.Title,
+				Systems:                      systemWithQuestionsCount,
+				CourseID:                     subject.CourseID,
+				QuestionsCount:               systemsQuestionsCount,
+				TraditionalQuestionsCount:    systemTraditionalsCount,
+				NextGenerationQuestionsCount: systemNextGenerationsCount,
+			})
+			if subject.Type == GENERAL_TYPE_TRADITIONAL {
+				TraditionalSubjects.Subjects = append(TraditionalSubjects.Subjects, subjectsWithQuestionsCount...)
+			} else {
+				NextGenerationSubjects.Subjects = append(TraditionalSubjects.Subjects, subjectsWithQuestionsCount...)
+			}
+		}
 		courseWithQuestionsCount := ConvertCourseToCourseWithQuestionsCounts(*userBoughtCourses[i].Course.ParentCourse)
 		totalTraditionalQuestions += courseWithQuestionsCount.TraditionalQuestionsCount
 		totalNextGenerationQuestions += courseWithQuestionsCount.NextGenerationQuestionsCount
@@ -214,10 +271,10 @@ func UserBoughtCoursesWithExpirationDateAndQuestionsCount(userID uint) (*[]Cours
 			}
 		}
 		userCourses = append(userCourses, CourseWithExpirationDateAndQuestionsCount{
-			ID:                           userBoughtCourses[i].ID,
-			Title:                        userBoughtCourses[i].Course.Title,
-			ExpirationDate:               userBoughtCourses[i].ExpirationDate, // most important part
-			Subjects:                     courseWithQuestionsCount.Subjects,   // subject is not from Subject model
+			ID:             userBoughtCourses[i].ID,
+			Title:          userBoughtCourses[i].Course.Title,
+			ExpirationDate: userBoughtCourses[i].ExpirationDate, // most important part
+			// Subjects:                     courseWithQuestionsCount.Subjects,   // subject is not from Subject model
 			Duration:                     userBoughtCourses[i].Course.ParentCourse.Duration,
 			QuestionsCount:               courseWithQuestionsCount.QuestionsCount,
 			TraditionalQuestionsCount:    totalTraditionalQuestions,
@@ -225,6 +282,8 @@ func UserBoughtCoursesWithExpirationDateAndQuestionsCount(userID uint) (*[]Cours
 			MarkedQuestionsCount:         markedCount,
 			CorrectQuestionsCount:        correctCount,
 			IncorrectQuestionsCount:      incorrectCount,
+			NextGenerationSubjects:       NextGenerationSubjects,
+			TraditionalSubjects:          TraditionalSubjects,
 		})
 	}
 	return &userCourses, nil
--- models/subject.model.go
+++ models/subject.model.go
@@ -1,12 +1,20 @@
 package models
 
+type QuestionGeneralType int8
+
+const (
+	GENERAL_TYPE_TRADITIONAL QuestionGeneralType = iota
+	GENERAL_TYPE_NEXT_GENERATION
+)
+
 type Subject struct {
 	ID    uint   `json:"id" gorm:"primary_key"`
 	Title string `json:"title"`
 	// relationships
-	CourseID uint      `json:"courseID"`
-	Course   *Course   `json:"course,omitempty"`
-	Systems  []*System `json:"systems" gorm:"foreignKey:SubjectID"`
+	CourseID uint                `json:"courseID"`
+	Course   *Course             `json:"course,omitempty"`
+	Systems  []*System           `json:"systems" gorm:"foreignKey:SubjectID"`
+	Type     QuestionGeneralType `json:"type"`
 }
 type SubjectWithSystems struct {
 	ID       uint      `json:"id" gorm:"primary_key"`
@@ -17,7 +25,7 @@ type SubjectWithSystems struct {
 type SubjectWithQuestionsCount struct {
 	ID                           uint                        `json:"id" gorm:"primary_key"`
 	Title                        string                      `json:"title"`
-	Systems                      []*SystemWithQuestionsCount `json:"systems"`
+	Systems                      []SystemWithQuestionsCount `json:"systems"`
 	CourseID                     uint                        `json:"courseID"`
 	QuestionsCount               int                         `json:"questionsCount"`
 	TraditionalQuestionsCount    int                         `json:"traditionalQuestionsCount"`
